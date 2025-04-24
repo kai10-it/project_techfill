9100d5c55beed665ae5506f377de66f4264ea9b3
kai10-it <kaitom.it.engineer@gmail.com>
2025-04-24 10:33:28 +0900

    ArticlesControllerに記事の一覧・詳細・投稿機能を追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,39 @@
 class ArticlesController < ApplicationController
+    # ログインしていない時は記事を投稿できないようにする
+    before_action :authenticate_user!, except: [:index, :show]
+
+    # 新しく投稿された順番で記事の一覧を表示する
+    def index
+        @articles = Article.all.order(created_at: :desc)
+    end
+
+    # 記事の内容を閲覧する
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    # ログイン中のユーザーが新しい記事を作成するための準備をする
+    def new
+        @article = current_user.articles.build
+    end
+
+    # フォームから送信された記事を保存し、保存後の処理を行う
+    def create
+        @article = current_user.articles.build(article_params)
+        # 記事の保存に成功したら、その記事のページに移動しメッセージを表示する
+        if @article.save
+            redirect_to @article, notice: "記事を投稿しました"
+        # 投稿失敗時にもう一度投稿フォームを表示する
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    # ここからはcontroller内だけで使う処理
+    private
+    
+    # 記事のタイトルと記事の内容のデータだけを受け取る処理
+    def article_params
+        params.require(:article).permit(:title, :body)
+    end
 end
