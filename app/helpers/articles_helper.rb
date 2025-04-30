b9f715708925eb609ee3a077a5999d96e473e564
kai10-it <kaitom.it.engineer@gmail.com>
2025-04-30 10:49:53 +0900

    {{単語}}を穴埋め表示に変換するヘルパーメソッドを追加

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,13 @@
 module ArticlesHelper
+    def render_article_body_with_blanks(body, mode)
+        # 穴埋め問題モードであるならば、{{単語}}で囲った部分を<span>タグに変換する
+        if mode == 'quiz'
+            body.gsub(/\{\{(.*?)\}\}/) do
+                "<span class='fill-blank' data-answer='#{$1}'>■■■■■</span>"
+            end.html_safe
+        # 閲覧モードであるならば、そのまま表示
+        else
+            simple_format(body)
+        end
+    end
 end
