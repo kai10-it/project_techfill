8eeac53542ef7bb6161ffd069baaf6ca4837b06f
kai10-it <kaitom.it.engineer@gmail.com>
2025-04-30 13:55:24 +0900

    CommentモデルにUser・Articleとの関連付けを追加"

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   # ユーザーは複数の記事を作成し、所有することができる
   # ユーザーが削除された時は、そのユーザーが作成した記事もすべて削除される
   has_many :articles, dependent: :destroy
+
+  # ユーザーは複数のコメントを行うことができる
+  # ユーザーが削除された時は、そのユーザーのコメントもすべて削除される
+  has_many :comments, dependent: :destory
 end
