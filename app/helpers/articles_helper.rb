3a69cf3e794aafcfc2750dc062d27123b864f118
kai10-it <kaitom.it.engineer@gmail.com>
2025-04-30 12:02:20 +0900

    穴埋め部分を<span>タグ＋CSSで表示するように変更

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,7 +3,7 @@ module ArticlesHelper
         # 穴埋め問題モードであるならば、{{単語}}で囲った部分を<span>タグに変換する
         if mode == 'quiz'
             body.gsub(/\{\{(.*?)\}\}/) do
-                "<span class='fill-blank' data-answer='#{$1}'>■■■■■</span>"
+                "<span class='fill-blank'>#{$1}</span>"
             end.html_safe
         # 閲覧モードであるならば、そのまま表示
         else
