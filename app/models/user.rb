1adbf25e990e37bb4ecf734693fe3c4057a9fb5a
kai10-it <kaitom.it.engineer@gmail.com>
2025-06-20 14:01:21 +0900

    Deviseの削除に伴う関連ファイルの整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         
   has_one_attached :avatar
 
   # ユーザーは複数の記事を作成し、所有することができる
