b930611aa0fb7b8641c64b09c781e0d4896d0795
kai10-it <kaitom.it.engineer@gmail.com>
2025-06-20 17:55:07 +0900

    ユーザー情報の登録成否を判定する処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-    def new        
+    def new
+        @user = User.new        
     end
 
     def create
         @user = User.new(name: params[:name], email: params[:email], password: params[:password])
-        @user.save
-        redirect_to("/")
+        if @user.save
+            redirect_to("/")
+        else
+            render("users/new")
+        end
     end
 end
