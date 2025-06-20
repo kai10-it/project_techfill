395b6d6be3d1637eb172c9fd27137becfc04b574
kai10-it <kaitom.it.engineer@gmail.com>
2025-06-20 15:04:00 +0900

    Usersテーブル作成のためのマイグレーション追加・更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_20_051627) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_20_055622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -55,6 +55,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_20_051627) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
