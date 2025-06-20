d43c94622182409eac4d4725b0f5039a206c9691
kai10-it <kaitom.it.engineer@gmail.com>
2025-06-20 16:43:42 +0900

    routes.rbで設定したサインアップ用ルートのURL名を変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "/" => "home#top"
 
   # サインアップのルート
-  get "users/new" => "users#new"
+  get "users/sinup" => "users#new"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
