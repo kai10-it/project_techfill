01b5d8100160b0dfed8b56535f48906c65203f68
kai10-it <kaitom.it.engineer@gmail.com>
2025-06-22 09:10:43 +0900

    ユーザー情報更新用のアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   post "users/create" => "users#create"
   # ユーザー情報のルート
   get "users/:id/edit" => "users#edit"
-   
+  # ユーザー情報の更新をするルート
+  post "users/:id/update" => "users#update"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
