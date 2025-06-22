3e36057cc74fdafb76a563b6c3f4f1c77a87c60b
kai10-it <kaitom.it.engineer@gmail.com>
2025-06-22 11:54:25 +0900

    UsersControllerのタイポを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
     def login
         @user = User.find_by(email: params[:email], password: params[:password])
         if @user
-            session[:usre_id] = @user.id
+            session[:user_id] = @user.id
             redirect_to("/")
         else
             render("users/check")
