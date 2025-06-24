2e03045addd11ce722ed9fa7eaa7fee467a61516
kai10-it <kaitom.it.engineer@gmail.com>
2025-06-24 11:09:46 +0900

    Dockerボリューム削除に伴いマイグレーションを再実行し、schema.rbを更新

--- db/schema.rb
+++ db/schema.rb
@@ -22,20 +22,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_23_090228) do
     t.string "user_id"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.text "content"
-    t.bigint "article_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password"
     t.string "image_name"
   end
-
-  add_foreign_key "comments", "articles"
 end
