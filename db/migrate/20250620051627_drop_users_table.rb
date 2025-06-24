582e3ec635a41429a9a19c9b4c2e935a94289c66
kai10-it <kaitom.it.engineer@gmail.com>
2025-06-24 10:55:41 +0900

    Dockerボリューム削除に伴い、不要なマイグレーションファイルを削除

--- db/migrate/20250620051627_drop_users_table.rb
+++
@@ -1,8 +0,0 @@
-class DropUsersTable < ActiveRecord::Migration[8.0]
-  def change
-    remove_reference :articles, :user rescue nil
-    remove_reference :comments, :user rescue nil
-
-    drop_table :users
-  end
-end
