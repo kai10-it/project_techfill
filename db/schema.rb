098015910ed16b77a7fb7d3aca73e4cb314e27b6
kai10-it <kaitom.it.engineer@gmail.com>
2025-06-26 10:37:51 +0900

    テーブル名を変更し、マイグレーションを更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_26_013149) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_26_013358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,7 +22,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_26_013149) do
     t.string "user_id"
   end
 
-  create_table "comments_tables", force: :cascade do |t|
+  create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.string "article_id"
     t.string "user_id"
