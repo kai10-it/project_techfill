b0318b2e03f026325dfebfe80820fbb092903c54
kai10-it <kaitom.it.engineer@gmail.com>
2025-06-26 10:35:14 +0900

    不要なCommentsテーブルを削除し、マイグレーションを更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_25_113612) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_26_013149) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,12 +20,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_25_113612) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_id"
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "comments_tables", force: :cascade do |t|
