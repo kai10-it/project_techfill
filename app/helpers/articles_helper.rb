03c66b116cf741d93ced4b829ea3775d7488d304
kai10-it <kaitom.it.engineer@gmail.com>
2025-06-30 10:05:30 +0900

    リファクタリングのため article_helper.rb の内容を全削除

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,13 +1,2 @@
 module ArticlesHelper
-    def render_article_body_with_blanks(body, mode)
-        # 穴埋め問題モードであるならば、{{単語}}で囲った部分を<span>タグに変換する
-        if mode == 'quiz'
-            body.gsub(/\{\{(.*?)\}\}/) do
-                "<span class='fill-blank'>#{$1}</span>"
-            end.html_safe
-        # 閲覧モードであるならば、そのまま表示
-        else
-            simple_format(body)
-        end
-    end
 end
