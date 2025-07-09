4677d8a6f32e70410beba6c7dd5ebb42da2b2719
kai10-it <kaitom.it.engineer@gmail.com>
2025-07-09 13:07:38 +0900

    RuboCopに従い comments_controller.rb のコードスタイルを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,10 @@
+# CommentsControllerはコメントに関する操作（新しいコメントの作成）を扱うコントローラ
 class CommentsController < ApplicationController
   before_action :authenticate_user
 
   def create
     @comment = Comment.new(comment: params[:comment], article_id: params[:id], user_id: @current_user.id)
-      @article = Article.find_by(id: params[:id])
-      if @comment.save
-        redirect_to("/articles/#{@article.id}/show")
-      end
+    @article = Article.find_by(id: params[:id])
+    redirect_to("/articles/#{@article.id}/show") if @comment.save
   end
 end
