4bf3d5960f847f84f1e50e9d37e7a2edfb0e853e
kai10-it <kaitom.it.engineer@gmail.com>
2025-07-10 13:06:39 +0900

    RuboCop に従いマイグレーションのコードスタイルを修正

--- db/migrate/20250626013149_drop_comments_table.rb
+++ db/migrate/20250626013149_drop_comments_table.rb
@@ -1,6 +1,10 @@
 # drop_comments_tableはcommentsテーブルを削除するマイグレーション
 class DropCommentsTable < ActiveRecord::Migration[8.0]
   def change
-    drop_table :comments;
+    drop_table :comments do |t|
+      t.text :content
+
+      t.timestamps
+    end
   end
 end
