d6ae84d7bfd31b2f38498232997c95ec8c2b3c25
kai10-it <kaitom.it.engineer@gmail.com>
2025-07-11 11:31:03 +0900

    RuboCop に従い users_controller.rb の処理をヘルパーメソッドで簡略化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(
-      name: params[:name],
-      email: params[:email],
-      password: params[:password],
-      image_name: 'default_user.jpg'
-    )
+    @user = build_user
 
     if @user.save
       redirect_to('/')
@@ -31,15 +26,8 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find_by(id: params[:id])
-    @user.name = params[:name]
-    @user.email = params[:email]
-    @user.password = params[:password]
-
-    if params[:image]
-      @user.image_name = "#{@user.id}.jpg"
-      image = params[:image]
-      File.binwrite("public/users_image/#{@user.image_name}", image.read)
-    end
+    assign_user_attributes(@user)
+    user_save_image(@user, params[:image]) if params[:image]
 
     if @user.save
       redirect_to('/')
@@ -74,5 +62,31 @@ class UsersController < ApplicationController
 
   def forbid_login_user
     redirect_to('/articles/index') if @current_user
+  end
+
+  private
+
+  # ユーザーの新規作成ヘルパーメソッド
+  def build_user
+    User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      image_name: 'default_user.jpg'
+    )
+  end
+
+  # ユーザー情報更新のヘルパーメソッド
+  def assign_user_attributes(user)
+    user.name = params[:name]
+    user.email = params[:email]
+    user.password = params[:password]
+  end
+
+  # ユーザー情報更新のヘルパーメソッド
+  # 画像を保存するためのメソッド
+  def user_save_image(user, image_param)
+    user.image_name = "#{user.id}.jpg"
+    File.binwrite("public/users_image/#{user.image_name}", image_param.read)
   end
 end
