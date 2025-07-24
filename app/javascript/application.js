636f32b0deee6e55abdb93a5342fc6eeee3fe82f
kai10-it <kaitom.it.engineer@gmail.com>
2025-07-24 14:47:56 +0900

    Flash メッセージを数秒後に自動で非表示にする処理を追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:load', () => {
+  const flashMessages = document.querySelectorAll('.flash');
+  flashMessages.forEach((message) => {
+    setTimeout(() => {
+      message.remove();
+    }, 3000);
+  });
+});
